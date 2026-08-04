d575e528da7689edee7d5d4ef4be0ebc8c5d915e
dnk31288 <dharmon@globalcommserv.com>
2026-08-04 15:47:04 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# GCS Website
+
+Upload all files and folders in this directory to the root of the GitHub repository. Enable GitHub Pages from the main branch and root folder.
---
+++ main.js
@@ -0,0 +1,1 @@
+const btn=document.querySelector('.menu-button');const nav=document.querySelector('.nav');if(btn&&nav){btn.addEventListener('click',()=>nav.classList.toggle('open'));}
